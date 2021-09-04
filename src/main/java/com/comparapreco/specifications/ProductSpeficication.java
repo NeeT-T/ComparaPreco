3764534ec45816f73b47df7b96685d8e8d56309c
Renato S. Oliveira <50246351+NeeT-T@users.noreply.github.com>
2021-09-04 10:28:01 -0300

    Foo

--- src/main/java/com/comparapreco/specifications/ProductSpeficication.java
+++ src/main/java/com/comparapreco/specifications/ProductSpeficication.java
@@ -1,5 +1,7 @@
 package com.comparapreco.specifications;
 
-public class ProductSpeficication {
-
+public class ProductSpeficication implements Specification<Ptoduct> {
+    private Sring name;
+    private Brand brand;
+    private category category;
 }
